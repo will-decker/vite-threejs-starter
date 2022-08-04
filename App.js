bbc5a8f0262f8fd60df456d195492a5715899ab1
willdecker <willdecker@gmail.com>
2022-08-04 02:20:15 -0400

    start building car lights

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import './styles.css';
 import * as THREE from 'three';
 import { BasicThreejsSetup } from './BasicThreejsSetup';
 import { Road } from './Road';
-
+import { CarLights } from './CarLights';
 export class App extends BasicThreejsSetup {
   constructor(container, options) {
     super(container);
-    this.camera.position.z = -4;
+    // this.camera.position.z = 50;
+    // Dp stuff
+    this.camera.position.z = -5;
     this.camera.position.y = 7;
     this.camera.position.x = 0;
 
     this.road = new Road(this, options);
+    this.leftLights = new CarLights(this, options);
+    console.log(this.leftLights);
   }
   loadAssets() {
     return new Promise((resolve, reject) => {
@@ -21,7 +25,11 @@ export class App extends BasicThreejsSetup {
     });
   }
   init() {
+    // this.scene.add(mesh);
+
     this.road.init();
+    this.leftLights.init();
+
     this.tick();
   }
   update(delta) {}
